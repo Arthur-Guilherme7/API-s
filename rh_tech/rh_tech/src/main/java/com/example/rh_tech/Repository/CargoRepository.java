908b39693183ca7e00ccb93a50902ac37125ee74
Arthur Caetano <arthurguilhermecaetano73@gmail.com>
2025-06-15 18:22:20 -0300

    Delete rh_tech/rh_tech directory

--- rh_tech/rh_tech/src/main/java/com/example/rh_tech/Repository/CargoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rh_tech.Repository;
-
-import com.example.rh_tech.Model.CargoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CargoRepository extends JpaRepository<CargoModel, Long> {
-}
